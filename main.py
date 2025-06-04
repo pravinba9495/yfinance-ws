63197cca176ca0819bda49be9394449e0b0c0408
Praveen Ravichandran <me@pravinba9495.dev>
2025-06-04 16:39:53 -0400

    chore: update logging format

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import yfinance as yf
 import redis
 import json
 import os
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y/%m/%d %H:%M:%S')
 
 r = redis.Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], db=0)
 
 def message_handler(message):
-    print(">> ", message['id'], message['price'],)
+    logging.info(message)
     r.publish("trades", json.dumps({
         "s": message['id'],
         "p": message['price']
